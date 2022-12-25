3395e5bd2aeb92f4825ed7d08077f574f62e8b00
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-25 12:12:50 -0800

    Implement Mine Page UI; Implement Register Page; Implement Login Page;

--- app/src/main/java/com/example/mymutao/fragment/MeFragment.java
+++ app/src/main/java/com/example/mymutao/fragment/MeFragment.java
@@ -1,21 +1,40 @@
 package com.example.mymutao.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.mymutao.LoginActivity;
 import com.example.mymutao.R;
 
 public class MeFragment extends Fragment {
+
+    private Button mBtnLogin;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mBtnLogin= (Button) getView().findViewById(R.id.btn_login);
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //登录
+                Intent login=new Intent(getActivity(), LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }
