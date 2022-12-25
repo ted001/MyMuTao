85c2132768f356b29ffd20d1e0e58d4c688b602d
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-24 23:12:21 -0800

    Add Splash Screen

---
+++ app/src/main/java/com/example/mymutao/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.mymutao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 2000);
+    }
+}
