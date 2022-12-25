8ce0893ecea8bfac7b9953fd7610dd4c89428d16
Zhiyi Jin <victoryjzy@gmail.com>
2022-12-25 13:22:54 -0800

    Implement Main Page UI

---
+++ app/src/main/java/com/example/mymutao/adapter/MainHeaderAdAdapter.java
@@ -0,0 +1,42 @@
+package com.example.mymutao.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class MainHeaderAdAdapter extends PagerAdapter {
+
+    protected Context context;
+    protected List<ImageView> images;
+
+    public MainHeaderAdAdapter(Context context, List<ImageView> images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return null != images ? images.size() : 0;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(images.get(position));
+        return images.get(position);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(images.get(position));
+    }
+}
--- app/src/main/java/com/example/mymutao/fragment/MainFragment.java
+++ app/src/main/java/com/example/mymutao/fragment/MainFragment.java
@@ -8,14 +8,49 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.mymutao.R;
+import com.example.mymutao.adapter.MainHeaderAdAdapter;
+import com.example.mymutao.adapter.MainMenuAdapter;
+import com.example.mymutao.util.DataUtil;
 
 public class MainFragment extends Fragment {
+    protected ViewPager mVPagerHeaderAd;//广告头
+    protected int[] icons = {R.mipmap.header_pic_ad1, R.mipmap.header_pic_ad2, R.mipmap.header_pic_ad1};
+    //菜单图标
+    protected int[] menuIons = {R.mipmap.menu_airport, R.mipmap.menu_car
+            , R.mipmap.menu_course, R.mipmap.menu_hatol,
+            R.mipmap.menu_nearby, R.mipmap.me_menu_go,
+            R.mipmap.menu_ticket, R.mipmap.menu_train};
+    protected String[] menus;
+    protected RecyclerView mRecycleViewMenu;//主菜单
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        menus = this.getActivity().getResources().getStringArray(R.array.main_menu);
+
+        mVPagerHeaderAd = (ViewPager) getView().findViewById(R.id.vpager_main_header_ad);
+        mRecycleViewMenu = (RecyclerView) getView().findViewById(R.id.recycleview_main_menu);
+
+
+        MainHeaderAdAdapter adapter = new MainHeaderAdAdapter(getActivity(), DataUtil.getHeaderAddInfo(getActivity(), icons));
+        mVPagerHeaderAd.setAdapter(adapter);
+
+        //菜单
+        //布局样式
+        mRecycleViewMenu.setLayoutManager(new GridLayoutManager(getActivity(), 4));
+        MainMenuAdapter mainMenuAdapter = new MainMenuAdapter(getActivity(), DataUtil.getMainMenus(menuIons, menus));
+        mRecycleViewMenu.setAdapter(mainMenuAdapter);
+
+    }
 }
